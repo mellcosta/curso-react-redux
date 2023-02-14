18a076a6d8240d3110caa747ffa698e36297efb9
mellcosta <m3licosta@gmail.com>
2023-02-14 11:23:49 +0000

    Componente Calculator

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import { Container } from "./Styled";
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <Container>
+        
+      </Container>
+    )
+  }
+}
---
+++ calculadora/src/main/Styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+  background-color: red;
+`;
