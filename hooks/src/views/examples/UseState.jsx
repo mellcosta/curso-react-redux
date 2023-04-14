ce21467dc145d53d2ce928bfd08aa35b8c86bf54
mellcosta <m3licosta@gmail.com>
2023-04-14 18:12:24 +0100

    UseState #01

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+    const [count, setCount] = useState(0)
     return (
         <div className="UseState">
             <PageTitle
@@ -11,6 +12,21 @@ const UseState = (props) => {
             />
 
             <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text"> {count} </span>
+                <div>
+                    <button className="btn" 
+                        onClick={() => setCount(currect => currect - 10)}> -10 </button>
+                    <button className="btn" 
+                        onClick={() => setCount(count - 1)}> -1 </button>
+                    <button className="btn" 
+                        onClick={() => setCount(count + 1)}> +1 </button>
+                    <button className="btn" 
+                        onClick={() => setCount(currect => currect + 10)}> +10 </button>
+                </div>
+                <button className="btn" 
+                        onClick={() => setCount(0)}>Reset</button>
+            </div>
 
             <SectionTitle title="Exercício #02" />
         </div>
