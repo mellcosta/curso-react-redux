52dfac2c989a785223de70c7d7917e40caa20f64
mellcosta <m3licosta@gmail.com>
2023-04-14 21:05:28 +0100

    Desafio UseEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 function calcFactorial (num) {
     const n = parseInt(num)
@@ -16,18 +17,36 @@ const UseEffect = (props) => {
         setFactorial(calcFactorial(number))
     }, [number])
 
+    //Desafio
+    const isPar = number % 2 === 0
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+
+            <SectionTitle title="Exemplo Factorial" />
             <div>
                 <span className="text">Factorial: </span>
                 <span className="text red"> {factorial === -1 ? 'Não existe' : factorial} </span>
             </div>
             <input type="number" className="input" value={number}
                 onChange={e => setNumber(e.target.value)}/>
+
+            <SectionTitle title="Desafio UseEffect" />
+            <div>
+                <span className="text">Estado: </span>
+                <span className="text red">
+                    {
+                    isPar ? <strong> Par</strong> : <strong> Ímpar</strong>
+                    }
+                </span>
+            </div>
+
+            <SectionTitle title="Resolução" />
+            
         </div>
     )
 }
