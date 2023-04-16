279502754e4c30069f49490dcf6c339c5dfa5ffe
mellcosta <m3licosta@gmail.com>
2023-04-16 22:35:56 +0100

    UseRef #02

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -6,9 +6,19 @@ const UseRef = (props) => {
     const [value1, setValue1] = useState('')
     const count = useRef(0)
 
+    const [value2, setValue2] = useState('')
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
     useEffect(function() {
         count.current = count.current + 1
-    }, [value1] )
+        myInput2.current.focus()
+    }, [value1])
+
+    useEffect(function() {
+        count.current ++
+        myInput1.current.focus()
+    }, [value2])
 
     return (
         <div className="UseRef">
@@ -20,13 +30,18 @@ const UseRef = (props) => {
             <SectionTitle title="Exercício #01" />
             <div>
                 <span className="text">Valor: </span>
-                <span className="text"> {value1}</span>
+                <span className="text"> {value1} [</span>
                 <span className="text red"> {count.current} </span>
-                <span className="text"> </span>
+                <span className="text">]</span>
             </div>
             <input type="text" className="input"
+                ref={myInput1}
                 value={value1} onChange={e => setValue1(e.target.value)} />
 
+            <SectionTitle title="Exercício #02" />
+            <input type="text" className="input"
+                ref={myInput2}
+                value={value2} onChange={e => setValue2(e.target.value)} />
         </div>
     )
 }
