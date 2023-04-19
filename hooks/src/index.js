d140afd63905836855cce2ad404218a2681ce55b
mellcosta <m3licosta@gmail.com>
2023-04-19 21:21:43 +0100

    UseContext (index.js)

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -2,9 +2,13 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import DataContext, { data } from './data/DataContext'
+
 import App from './views/App'
 
 ReactDOM.render(
-    <App />,
+    <DataContext.Provider value={data}> 
+        <App />
+    </DataContext.Provider>,
     document.getElementById('root')
 )
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import DataContext from '../../data/DataContext'
 
 const UseContext = (props) => {
+
+    const context = useContext (DataContext)
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <span className="text"> {context.text} </span>
+            <span className="text"> {context.number} </span>
         </div>
     )
 }
